f7ccc28cc9817f0b80f26517b700df3de1925372
athulpm <97athulpm@gmail.com>
2023-01-15 21:18:55 +0530

    made login mandatory to view store

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import Header from "./components/Header/Header";
 
 import "./App.css";
@@ -10,34 +10,39 @@ import Home from "./Pages/Home";
 import ContactUs from "./Pages/ContactUs";
 import ProductDetail from "./components/Products/ProductDetail";
 import AuthPage from "./Pages/AuthPage";
+import AuthContext from "./store/auth-context";
 
 function App() {
+  const authCtx = useContext(AuthContext);
+
   return (
     <div className="Ecommerce-container">
       <CartProvider>
         <Header />
         <Switch>
-        <Route path="/" exact>
-          <Redirect to="/home" />
-        </Route>
-        <Route path="/home">
-          <Home />
-        </Route>
-        <Route path="/store" exact>
-          <Products />
-        </Route>
-        <Route path="/store/:productId">
-          <ProductDetail/>
-        </Route>
-        <Route path="/about">
-          <AboutUs />
-        </Route>
-        <Route path="/login">
-          <AuthPage />
-        </Route>
-        <Route path="/contact-us">
-          <ContactUs />
-        </Route>
+          <Route path="/" exact>
+            <Redirect to="/home" />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/store" exact>
+            {authCtx.isLoggedin && <Products />}
+            {!authCtx.isLoggedin && <Redirect to="/login" />}
+          </Route>
+          <Route path="/store/:productId">
+            {authCtx.isLoggedin && <ProductDetail />}
+            {!authCtx.isLoggedin && <Redirect to="/login" />}
+          </Route>
+          <Route path="/about">
+            <AboutUs />
+          </Route>
+          <Route path="/login">
+            <AuthPage />
+          </Route>
+          <Route path="/contact-us">
+            <ContactUs />
+          </Route>
         </Switch>
       </CartProvider>
     </div>
