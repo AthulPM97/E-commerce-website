36358cdda5188d3ac8f4ab188e23fcf505bafdbd
athulpm <97athulpm@gmail.com>
2023-02-02 22:22:38 +0530

    Refined

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import App from "./App";
 // react bootstrap configuration
 import "../node_modules/react-bootstrap/dist/react-bootstrap.min";
 import "bootstrap/dist/css/bootstrap.css";
+
 import { AuthProvider } from "./store/auth-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
